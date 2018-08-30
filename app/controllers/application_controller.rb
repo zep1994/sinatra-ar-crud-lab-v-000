8f18d6314475a525fd69577c14a33f4b8a1b67aa
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-30 14:03:31 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,50 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
+    redirect '/posts'
   end
+  
+  #CREATE
+  get '/posts/new' do 
+    erb :'new.erb'
+  end
+  
+  post '/posts' do
+    @post = Post.create(name: params[:name], content: params[:content])
+    erb :'show.html'
+  end
+  
+  #READ 
+  post '/posts' do
+    @post = Post.all 
+    erb :'index.html'
+  end
+  
+  post '/posts/:id' do
+    @post = Post.find(params[:id]) 
+    erb :'show.html'
+  end
+  
+  #UPDATE
+  post '/posts/:id/edit' do
+    @post = Post.find(params[:id]) 
+    erb :'edit.html'
+  end
+  
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :'show.html'
+    
+  end
+   #DELETE 
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.delete
+    erb :'delete.html'
+  end
+
+  
 end
