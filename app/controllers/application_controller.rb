77716a2515192cc5dcb7c708bbbb7ccbf22e7733
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-30 14:09:14 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,49 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
+    redirect '/posts'
   end
+   #CREATE
+  get '/posts/new' do
+    erb :'new.html'
+  end
+
+  post '/posts' do
+    @post = Post.create(name: params[:name], content: params[:content])
+    erb :'show.html'
+  end
+   #READ 
+  get '/posts' do
+    @posts = Post.all
+    erb :'index.html'
+  end
+   
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :'show.html'
+  end
+   #UPDATE
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :'edit.html'
+  end
+  
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :'show.html'
+    #shortform update
+      #@post = Post.find(params[:id])
+      #@post.update(name: params[:name], content: params[:content])
+  end
+   #DELETE 
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.delete
+    erb :'delete.html'
+  end
+
+  
 end
